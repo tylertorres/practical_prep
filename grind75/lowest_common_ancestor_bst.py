f21fb1722a073bec9cc6914f9c89108a7f5a526b
Ty <tylertorresp@gmail.com>
2023-07-25 14:37:44 -0700

    Updated and more concise solution for lca of a bst

--- grind75/lowest_common_ancestor_bst.py
+++ grind75/lowest_common_ancestor_bst.py
@@ -5,13 +5,16 @@
 #         self.left = None
 #         self.right = None
 
+
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 
-'TreeNode') -> 'TreeNode':
+    def lowestCommonAncestor(
+        self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
+    ) -> "TreeNode":
         evaluation, ancestor = dfs(root, p, q)
         return ancestor
-    
-def dfs(root: 'TreeNode', p: 'TreeNode', q: 'TreeNode'):
+
+
+def dfs(root: "TreeNode", p: "TreeNode", q: "TreeNode"):
     if root is None:
         return False, None
 
@@ -20,15 +23,26 @@ def dfs(root: 'TreeNode', p: 'TreeNode', q: 'TreeNode'):
 
     if left_ancestor is not None or right_ancestor is not None:
         return True, left_ancestor if left_ancestor else right_ancestor
-    
+
     child = False
     isAncestorChild = root.val == p.val or root.val == q.val
 
-
     if left or right:
         child = True
-        
+
     if (child and isAncestorChild) or (left and right):
         return True, root
-    
+
     return child or isAncestorChild, None
+
+
+def lowestCommonAncestorV2(
+    self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
+) -> "TreeNode":
+    while True:
+        if root.val > p.val and root.val > q.val:
+            root = root.left
+        elif root.val < p.val and root.val < q.val:
+            root = root.right
+        else:
+            return root
